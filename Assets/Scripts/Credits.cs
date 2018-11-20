77a007a14463a86335b792f5d5ae085207be8a10
esinkalay <esin@tlu.ee>
2018-11-20 16:12:38 +0200

    Play Again feature added

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour {
 
 	public void Quit ()
 	{
+		Application.Quit();
+	}
 
+	public void PlayAgain ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
 	}
 
 }
